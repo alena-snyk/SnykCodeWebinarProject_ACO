808d76d5377606a37c051d43af571306c3776ab1
JennySnyk <jennifer.granja@snyk.io>
2022-01-17 18:53:57 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# DemoCarSharing
+***Demo for Static Program Analysis*** Simulates a car sharing backend / API / WebSite
+
+Based on simple express API server here https://www.robinwieruch.de/node-express-server-rest-api
+
+
+Test service
+curl -X GET http://localhost:80/vehicles/KIC-123
+
+curl -X GET http://localhost:80/vehicles/list
+
---
+++ dbInterface/vehicleDb.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql");
+
+var dbCon = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
+  });
+
+function getVehicleList() {
+    return dbCon.connect(function(err) {
+        if(err) throw err;
+        dbCon.query("SELECT LICENSE FROM VEHICLES", function (err, result, fields) {
+            if(err) throw err;
+            return result;
+        })
+    })
+}
+
+function getVehicleDetails(license) {
+    return dbCon.connect(function(err) {
+        if(err) throw err;
+        var query = "SELECT * FROM VEHICLES WHERE LICENSE LIKE " + license;
+        dbCon.query(query, function (err, result, fields) {
+            if(err) throw err;
+            return result;
+        })
+    })
+}
+
+module.exports = {
+    getVehicleList : getVehicleList,
+    getVehicleDetails : getVehicleDetails,
+}
